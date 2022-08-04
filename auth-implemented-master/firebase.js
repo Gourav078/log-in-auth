2ce3674370c696cbfd9f719c1d88157ad5f1c86b
Gourav078 <97431146+Gourav078@users.noreply.github.com>
2022-08-04 23:18:20 +0530

    Add files via upload

---
+++ auth-implemented-master/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJOtbbBWCRiotzCy63Z_tauQJjTb_FuZk",
+    authDomain: "form-401ec.firebaseapp.com",
+    databaseURL: "https://form-401ec-default-rtdb.firebaseio.com",
+    projectId: "form-401ec",
+    storageBucket: "form-401ec.appspot.com",
+    messagingSenderId: "66853234895",
+    appId: "1:66853234895:web:5bb6db6d5f3b1640826fba"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
